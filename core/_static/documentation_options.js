5a654b310f4a762fc9bb89d62f4ad4a18f369f7b
702fbtngus <702fbtngus@users.noreply.github.com>
2025-02-18 04:49:14 +0000

    deploy: 9f2d6bfd78130bb4fdd10ff21729e8d40ef27097

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0 (Draft 2025-02-17)',
+    VERSION: '3.0 (Draft 2025-02-18)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
