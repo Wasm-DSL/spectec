fc9df944743f15414c434f56378c6eff79cf496c
rossberg <rossberg@users.noreply.github.com>
2025-03-17 16:13:24 +0000

    deploy: f41000b195c1cd6e25286a0616b6036feb433c6f

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0 (Draft 2025-03-12)',
+    VERSION: '3.0 (Draft 2025-03-17)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
