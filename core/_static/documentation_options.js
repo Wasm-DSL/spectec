0c8a4e685a571e709631b389fbb4b5a2d94741ca
sukyoung <sukyoung@users.noreply.github.com>
2025-03-24 23:32:23 +0000

    deploy: 56b855d9a2252e7b4238ec92c319ccac4d28ecbc

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0 (Draft 2025-03-18)',
+    VERSION: '3.0 (Draft 2025-03-24)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
