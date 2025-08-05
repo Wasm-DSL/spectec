5f12048400aa94a27405a34685173afae15e3ec7
f52985 <f52985@users.noreply.github.com>
2025-08-05 11:12:29 +0000

    deploy: 99587f6fe03e6227a4195f87de42fbf2a5041b30

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0 (Draft 2025-08-04)',
+    VERSION: '3.0 (Draft 2025-08-05)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
