aa25e38cf78283121d1e2ec054bf46b7518f2e19
DCupello1 <DCupello1@users.noreply.github.com>
2025-10-30 11:20:11 +0000

    deploy: fbde36bfcece56ac44847d9f3309570440464f1f

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0 (Draft 2025-10-17)',
+    VERSION: '3.0 (Draft 2025-10-30)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
