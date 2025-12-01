3acf38ff07e46a268dd238c79427a45b04675d60
nomeata <nomeata@users.noreply.github.com>
2025-12-01 11:32:45 +0000

    deploy: 938f2db9213237fd6ef84127bb376966cc416bdd

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '3.0 (Draft 2025-11-19)',
+    VERSION: '3.0 (Draft 2025-12-01)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
