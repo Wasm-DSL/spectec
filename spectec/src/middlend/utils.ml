c5334ccff5625f563fed1d8a09ef179e34b1c17e
DCupello1 <dc1020@ic.ac.uk>
2026-01-20 14:51:39 +0000

    Made fallthrough only when otherwise is present. Refactored a bit using new traversal method

--- spectec/src/middlend/utils.ml
+++ spectec/src/middlend/utils.ml
@@ -31,12 +31,12 @@ and reduce_inst_alias env args inst base_typ =
     ) 
   | _ -> base_typ
 
-let is_part_of_bind (free_set : Free.sets) b =
+let is_part_of_quant (free_set : Free.sets) b =
   match b.it with
-  | ExpB (id, _) -> Free.Set.mem id.it free_set.varid 
-  | TypB id -> Free.Set.mem id.it free_set.typid
-  | DefB (id, _, _) -> Free.Set.mem id.it free_set.defid
-  | GramB (id, _, _) -> Free.Set.mem id.it free_set.gramid
+  | ExpP (id, _) -> Free.Set.mem id.it free_set.varid 
+  | TypP id -> Free.Set.mem id.it free_set.typid
+  | DefP (id, _, _) -> Free.Set.mem id.it free_set.defid
+  | GramP (id, _, _) -> Free.Set.mem id.it free_set.gramid
 
 let generate_var ids id =
   let start = 0 in
